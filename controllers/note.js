73289beda1048951748beae8d0b66f450f890222
Amit Singh Rana <ranaa73000@gmail.com>
2023-05-06 22:09:41 +0530

    Basic logic complete for note route

--- controllers/note.js
+++ controllers/note.js
@@ -1,19 +1,68 @@
+// Import files-functions
+import Note from "../models/note.js";
+
 export const getAllNotes = async (req, res) => {
-  res.send("Get all notes");
+  try {
+    const notesList = await Note.find();
+
+    return res.status(200).json({ success: true, notes: notesList });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err.message });
+  }
 };
 
 export const createNewNote = async (req, res) => {
-  res.send("Create new note");
+  try {
+    const newNote = new Note(req.body);
+
+    await newNote.save();
+
+    return res.status(201).json({ success: true, note: newNote });
+  } catch (err) {
+    res.status(400).json({ success: false, error: err.message });
+  }
 };
 
 export const getSingleNote = async (req, res) => {
-  res.send("Get single note");
+  try {
+    const foundNote = await Note.findById(req.params.id);
+
+    if (!foundNote) throw Error("Note not found");
+
+    return res.status(200).json({ success: true, note: foundNote });
+  } catch (err) {
+    res.status(404).json({ success: false, error: err.message });
+  }
 };
 
 export const editNote = async (req, res) => {
-  res.send("Edit Single Note");
+  try {
+    const editNote = {};
+    if (req.body.title) editNote.title = req.body.title;
+    if (req.body.description) editNote.description = req.body.description;
+
+    const editedNote = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: editNote },
+      { returnDocument: "after" }
+    );
+
+    if (!editedNote) throw Error("Note not found");
+
+    return res.status(200).json({ success: true, note: editedNote });
+  } catch (err) {
+    res.status(404).json({ success: false, error: err.message });
+  }
 };
 
 export const deleteNote = async (req, res) => {
-  res.send("Delete a note");
+  try {
+    const foundNote = await Note.findByIdAndDelete(req.params.id);
+
+    if (!foundNote) throw Error("Note not found");
+
+    return res.status(200).json({ success: true, note: foundNote });
+  } catch (err) {
+    res.status(404).json({ success: false, error: err.message });
+  }
 };
