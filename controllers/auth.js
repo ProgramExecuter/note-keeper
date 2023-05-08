4e9c97ef229006523827c5ab9169dd87967d103b
Amit Singh Rana <ranaa73000@gmail.com>
2023-05-08 21:31:28 +0530

    Added user signup route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,7 @@
 export const loginUser = async (req, res) => {
   res.send("Login User");
 };
+
+export const signupUser = async (req, res) => {
+  res.send("Signup User");
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,11 @@
 import express from "express";
 
 // Import files-functions
-import { loginUser } from "../controllers/auth.js";
+import { loginUser, signupUser } from "../controllers/auth.js";
 
 const router = express.Router();
 
-// Login route
 router.post("/login", loginUser);
+router.post("/signup", signupUser);
 
 export default router;
